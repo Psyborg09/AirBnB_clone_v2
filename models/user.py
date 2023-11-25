f75ccb32bc627f40c47b4a793168c5285f9c5ab1
kelvin <kelvin4stunt@gmail.com>
2023-11-25 21:14:43 +0100

    up

--- models/user.py
+++ models/user.py
@@ -1,12 +1,23 @@
 #!/usr/bin/pyhton3
 """This module defines a class `user`"""
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     '''Defines a user'''
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    def __str__(self):
+        """Return the string representation of the object."""
+        state_dict = self.__dict__.copy()
+        # Remove the '_sa_instance_state' attribute
+        state_dict.pop('_sa_instance_state', None)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, state_dict)
+
