2c4ae56e8c45552a2c52b9391d624f2b398492fa
kelvin <kelvin4stunt@gmail.com>
2023-12-11 15:45:26 +0100

    up

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# this script sets up your web servers for the deployment of web_static
+
+sudo apt-get update
+sudo apt-get -y install nginx
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
+echo "Hello, this is a test." | sudo tee /data/web_static/releases/test/index.html
+# Create symbolic link
+sudo rm -f /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+# Give ownership to the ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
+# Update Nginx configuration
+nginx_config="/etc/nginx/sites-available/default"
+sudo sed -i "/^server_name _;/a location /hbnb_static/ { alias /data/web_static/current/; }" $nginx_config
+sudo service nginx restart
