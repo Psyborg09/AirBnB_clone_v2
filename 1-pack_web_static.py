29f45ebb44db24c3df85a0fa23acadac14337934
kelvin <kelvin4stunt@gmail.com>
2023-12-27 13:09:08 +0100

    up

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,17 @@
-#!/usr/bin/python3
-from fabric import task
+#!/usr/bin/env python3
+"""Generates a .tgz archive from the contents of the web_static folder."""
+from fabric.api import *
 from datetime import datetime
 import os
 
-
-@task
-def do_pack(c):
+def do_pack():
     """Generates a .tgz archive from the contents of the web_static folder."""
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_name = "web_static_{}.tgz".format(timestamp)
-    archive_path = os.path.join("versions", archive_name)
-    c.local("mkdir -p versions")
-    result = c.local("tar -cvzf {} web_static".format(archive_path))
-    if result.failed:
-        return None
-    else:
-        return archive_path
+    local('sudo mkdir -p versions')
+    t = datetime.now()
+    t_str = t.strftime('%Y%m%d%H%M%S')
+    local("sudo tar -cvzf versions/web_static_{}.tgz web_static".format(t_str))
+
+    f_path = "versions/web_static_{}.tgz".format(t_str)
+    f_size = os.path.getsize(f_path)
+    print(f"web_static packed: {f_path} -> {f_size}Bytes")
+    return f_path
